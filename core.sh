2b633d1395affc292fe5c3287857a0e2ae076389
Patrick Henegan <32147213+PHenegan@users.noreply.github.com>
2022-08-24 14:01:08 -0400

    Create core.sh
    Core system files I use a lot

---
+++ core.sh
@@ -0,0 +1,20 @@
+pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+
+yay -S --needed python python-pip \
+                openjdk-jre openjdk-jre11 openjdk-jre8 \
+                nano vim \
+                firefox brave-bin \
+                discord \
+                zoom \
+                teams \
+                neofetch \
+                htop \
+                balena-etcher \
+                libmtp \
+                flatpak \
+                reflector reflector-simple \
+                ttf-liberation noto-fonts noto-fonts-emoji
