e89766e0c938dbf4a4db59f93dcd40f2353b146b
Patrick Henegan <32147213+PHenegan@users.noreply.github.com>
2022-08-24 14:35:40 -0400

    Create devel.sh

---
+++ devel.sh
@@ -0,0 +1,9 @@
+yay -S --needed intellij-idea-community-edition \ #intellij-idea-ultimate-edition 
+                jdk-openjdk jdk11-openjdk jdk8-openjdk \
+                code \
+                rustup \
+                qtcreator \
+                git \
+                github-cli \
+                cmake
+pip3 install numpy selenium sympy
