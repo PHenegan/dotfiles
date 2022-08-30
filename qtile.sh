7067cf78ccad3794c74be214c223be4cf5ad4cb8
Patrick Henegan <32147213+PHenegan@users.noreply.github.com>
2022-08-29 22:55:34 -0400

    Create qtile.sh

---
+++ qtile.sh
@@ -0,0 +1,11 @@
+yay -S --needed qtile qtile-extras-git \
+                rofi \
+                eww \
+                xarchiver zip unzip \
+                vlc \
+                picom \
+                xterm kitty \
+                nautilus thunar \
+                gedit \
+                bluman blueman-applet \
+                network-manager-applet \
