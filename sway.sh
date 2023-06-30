1d53aecef3cccb7c19cd66094fd6b19c986393cd
Patrick Henegan <32147213+PHenegan@users.noreply.github.com>
2023-06-30 19:23:06 -0400

    Update sway.sh

--- sway.sh
+++ sway.sh
@@ -1,1 +1,12 @@
-
+yay -S --needed sway swayidle swaybg \
+                rofi \
+                eww \
+                dunst \
+                xarchiver \
+                vlc \
+                hyprland \
+                xterm kitty \
+                nautilus thunar \
+                gedit \
+                blueman \
+                network-manager-applet \
