4821a0a51c4deab4300afd88f011a9eb967bcbc7
Patrick Henegan <phenegan03@gmail.com>
2026-02-06 00:08:01 -0500

    fix(porygon2): standard location for wallpaper and bar-icon

--- devices/porygon2/niri.nix
+++ devices/porygon2/niri.nix
@@ -2,26 +2,26 @@
 {
   imports = [ ../../shared/home/niri.nix ];
 
-  home.file.".config/niri/scripts/wallpaper.sh" = {
-    executable = true;
-    text = ''
+  home.file = {
+    # TODO: This can be modular with a config specifying the background file string
+    "config/niri/scripts/wallpaper.sh" = {
+      executable = true;
+      text = ''
       #!/bin/sh
       background_file=~/.dotfiles/assets/porygon-background.png
       swaybg -i $background_file -m fill
-    '';
-  };
+      '';
+    };
 
-  home.file.".config/niri/scripts/audio_devices.sh" = {
-    text = ''
-      #!/bin/sh
-      sleep 2 && noisetorch -i -s alsa_input.usb-MV-SILICON_fifine_Microphone_20190808-00.analog-stereo -t 80
-    '';
-    executable = true;
-  };
 
-  # NOTE: You cannot do ".source" directly because the whole config object
-  # Must be overriden
-  home.file.".config/niri/device.kdl" = {
-    source = ../config/niri/device.kdl;
+    # NOTE: You cannot do ".source" directly because the whole config object
+    # Must be overriden
+    ".config/niri/device.kdl" = {
+      source = ../config/niri/device.kdl;
+    };
+
+    # TODO: decide if this goes here or in home.nix
+    "Pictures/Wallpapers/porygon-background.png".source = ../../assets/porygon-background.png;
+    "Pictures/icons/bar-icon.png".source = ../../assets/icons/porygon.png;
   };
 }
