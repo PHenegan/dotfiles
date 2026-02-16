98041e04e2b1600e084a74b86e9cdf3e6a741851
Patrick Henegan <phenegan03@gmail.com>
2026-02-16 13:12:15 -0500

    fix(emu): syntax error in emulation nix file

--- shared/system/games/emu.nix
+++ shared/system/games/emu.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs [
+  environment.systemPackages = with pkgs; [
     # unstable.yuzu-mainline # F in the chat Yuzu, you will be missed
     # TODO: Find a way to get a yuzu or ryujinx fork in here
     cemu
